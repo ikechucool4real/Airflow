d6dea7d2411a32d2976a7a91e13dd8c0227fe7f2
Kaychucool <43006872+ikechucool4real@users.noreply.github.com>
2024-04-09 00:42:18 +0100

    Update Build2.sh

--- Build2.sh
+++ Build2.sh
@@ -6,5 +6,5 @@ for file in $(sudo docker exec airflow-airflow-worker-1 ls /opt/airflow/StarSche
     sudo docker cp airflow-airflow-worker-1:/opt/airflow/StarSchema/$file ./StarSchema/
 done
 
-sudo gsutil cp -r ./Staging/* gs://Airflow/Staging/
-sudo gsutil cp -r ./StarSchema/* gs://Airflow/StarSchema/
+sudo gsutil cp -r ./Staging/* gs://ike-airflow/Staging/
+sudo gsutil cp -r ./StarSchema/* gs://ike-airflow/StarSchema/
